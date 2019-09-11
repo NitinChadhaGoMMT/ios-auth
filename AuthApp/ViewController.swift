41ba8d54b07c3a6d7750401ed49e4c123d14fb30
Nitin Chadha <nitin.chadha@go-mmt.com>
2019-09-11 12:20:32 +0530

    Project setup

---
+++ AuthApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AuthApp
+//
+//  Created by Nitin Chadha on 11/09/19.
+//  Copyright © 2019 ibibo web Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+import Dip
+import NetworkLayerFramework
+import DependencyFramework
+import AuthModule
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AuthModule/Auth/Auth.swift
@@ -0,0 +1,16 @@
+//
+//  Auth.swift
+//  AuthModule
+//
+//  Created by Nitin Chadha on 11/09/19.
+//  Copyright © 2019 ibibo web Pvt Ltd. All rights reserved.
+//
+
+import Foundation
+
+public class Auth {
+    
+    public init() {
+        
+    }
+}
